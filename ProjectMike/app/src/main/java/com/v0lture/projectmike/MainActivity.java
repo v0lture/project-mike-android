c147330499030d0493d1fa6b73bc18c24f11298d
De'Angelo Grant <dagrant82@gmail.com>
2017-07-22 15:39:21 -0400

    Gradle acting funky
    Fix gradle bug

--- ProjectMike/app/src/main/java/com/v0lture/projectmike/MainActivity.java
+++ ProjectMike/app/src/main/java/com/v0lture/projectmike/MainActivity.java
@@ -1,22 +1,59 @@
 package com.v0lture.projectmike;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Switch;
+import android.widget.Toast;
+
+//import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-com.google.firebase.iid.FirbaseListAdapter<String> myAdapter;
 
+
+//    private static final int SIGN_IN_REQUEST_CODE = 0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRef
+       /* if(FirebaseAuth.getInstance().getCurrentUser()==null){
+            //Start sign In - Sign Up Activity
+            startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder().build(),SIGN_IN_REQUEST_CODE
+            );
+        }else{
+            //User already in,
+            //Display Welcome
+            Toast.makeText(this, "Welcome"+ FirebaseAuth.getInstance().getCurrentUser().getDisplayName(),Toast.LENGTH_LONG).show();
+            //Load Chat
+            displayMessages();
+        }*/
     }
+
+    private void displayMessages(){
+
+    }
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode == SIGN_IN_REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this, "Successfully signed in. Welcome!",Toast.LENGTH_LONG).show();
+                displayMessages();
+            }else{
+                Toast.makeText(this, "Sorry, we couldn't sign you in. Please Try again later.", Toast.LENGTH_LONG).show();
+                //Close
+                finish();
+            }
+        }
+    }*/
 
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater menuInflater = getMenuInflater();
@@ -24,11 +61,14 @@ com.google.firebase.iid.FirbaseListAdapter<String> myAdapter;
 
         return super.onCreateOptionsMenu(menu);
     }
-  /*  public boolean onOptionsItemSelected (final MenuItem Item){
+    public boolean onOptionsItemSelected (final MenuItem Item){
          switch(Item.getItemId()) {
-             case findViewById(R.id.me) :
-
-
+             case R.id.me:
+                 return true;
+             case R.id.you:
+                 return true;
+             default:
+                 return super.onOptionsItemSelected(Item);
         }
-    }*/
+    }
 }
